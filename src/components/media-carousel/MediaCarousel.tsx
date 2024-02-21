11ac27759450e67275f4089cb904e4b971f9d75f
John Hillert <john.hillert@gmail.com>
2024-02-21 10:02:04 -0600

    ...

--- src/components/media-carousel/MediaCarousel.tsx
+++ src/components/media-carousel/MediaCarousel.tsx
@@ -2,16 +2,31 @@ import { Options } from '@splidejs/splide';
 import { Video } from '@splidejs/splide-extension-video';
 // @ts-expect-error - dependency bug
 import { Splide, SplideSlide, SplideTrack } from '@splidejs/react-splide';
-import { generateSlides } from './utils';
+import { styled } from 'styled-components';
+import ReactPlayer from 'react-player';
 
-const MediaCarousel = () => {
+const PlayerContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const Player = styled(ReactPlayer)``;
+
+type Props = {
+  data: {
+    src: string;
+    alt: string;
+  }[];
+  loading: boolean;
+};
+
+const MediaCarousel = ({ data, loading }: Props) => {
   const options = {
     type: 'loop',
     gap: '1rem',
     autoplay: true,
     pauseOnHover: false,
     resetProgress: false,
-    height: '15rem',
   };
 
   return (
@@ -23,9 +38,11 @@ const MediaCarousel = () => {
       >
         <div style={{ position: 'relative' }}>
           <SplideTrack>
-            {generateSlides().map((slide) => (
+            {data.map((slide) => (
               <SplideSlide key={slide.src}>
-                <img src={slide.src} alt={slide.alt} />
+                <PlayerContainer>
+                  <Player url={slide.src} width="100%" controls={true} />
+                </PlayerContainer>
               </SplideSlide>
             ))}
           </SplideTrack>
